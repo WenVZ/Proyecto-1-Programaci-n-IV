92e49371c34d3d5d5b60b2e176dbeb4a48dcae13
unknown <wuendolinvalverde@gmail.com>
2026-06-12 22:26:34 -0600

    Guardando cambio

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,13 +5,14 @@ import { getSession } from "../services/authService";// IMPORTA FUNCIoN PARA OBT
 import {  Briefcase,  AlertTriangle,  Calendar,  Ticket,  ChevronLeft,  ChevronRight, ArrowUpDown,  ClipboardList } from "lucide-react";
 import { useReactTable, getCoreRowModel,getPaginationRowModel, getSortedRowModel, flexRender,} from "@tanstack/react-table";
 
-const JSONBIN_URL = `https://api.jsonbin.io/v3/b/${import.meta.env.VITE_JSONBIN_EMPRENDIMIENTOS_BIN_ID}`;
+const JSONBIN_URL = "https://api.jsonbin.io/v3/b/6a13f183ee5a733b1216ab4c";
 const JSONBIN_HDRS = {
   headers: {
     "Content-Type": "application/json",
-    "X-Master-Key": import.meta.env.VITE_JSONBIN_EMPRENDIMIENTOS_KEY,
+    "X-Master-Key": "$2a$10$Yj0n3PMXBv59/6uS6NvE1O2NARQX.kOKxPn9cFJB5RdfDo2WkbQp2",
   },
 };
+
 const FIREBASE_URL = "https://proyecto1prograiv-default-rtdb.firebaseio.com";
 
 const ACCENT_VARIANTS = {
